1e3a171d117d0b81652590817a06dfcb37273c77
ma2Cta <mtstdisk@gmail.com>
2023-09-10 11:52:05 +0900

    add basic auth

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  if (req.nextUrl.pathname.startsWith("/")) {
+    const basicAuth = req.headers.get("authorization");
+
+    if (basicAuth) {
+      const auth = basicAuth.split(" ")[1];
+      const [user, pwd] = Buffer.from(auth, "base64").toString().split(":");
+
+      if (user === "nishio" && pwd === "daiki") {
+        return NextResponse.next();
+      }
+    }
+
+    return new Response("Auth required", {
+      status: 401,
+      headers: {
+        "WWW-Authenticate": 'Basic realm="Secure Area"',
+      },
+    });
+  }
+}
