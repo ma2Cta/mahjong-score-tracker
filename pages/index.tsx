43d7d543be087816319093f729876fb454d7bb8f
ma2Cta <mtstdisk@gmail.com>
2023-10-28 18:32:13 +0900

    install tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <main>
-        <h1>mahjong score tracker</h1>
+        <h1 className="text-3xl font-bold underline">mahjong score tracker</h1>
         <p>麻雀のスコアを記録するアプリです。</p>
         <Link href="/sessions">セッション一覧</Link>
       </main>
