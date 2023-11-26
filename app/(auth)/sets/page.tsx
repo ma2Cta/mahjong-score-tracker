3a126b1d6110fa89c28d51d5d92c0f5d61892a18
ma2Cta <mtstdisk@gmail.com>
2023-11-26 15:38:29 +0900

    Adjust

--- app/(auth)/sets/page.tsx
+++ app/(auth)/sets/page.tsx
@@ -1,31 +1,9 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import { Set } from "@/app/_types/set";
-import useSWR, { mutate } from "swr";
 import SetList from "@/app/_components/set/SetList";
 import { Button } from "@/app/_components/ui/button";
 import TypographyH2 from "@/app/_components/ui/TypographyH2";
 
 const Sets: React.FC = () => {
-  const { data, error, isLoading } = useSWR("/api/sets");
-  const [sets, setSets] = useState<Set[]>([]);
-  useEffect(() => {
-    if (data) {
-      setSets(data.sets);
-    }
-    mutate("/api/sets");
-  }, [data]);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
   return (
     <div>
       <div className="flex justify-between items-center">
@@ -34,7 +12,7 @@ const Sets: React.FC = () => {
           <Button>新しいセットを作成</Button>
         </Link>
       </div>
-      <SetList sets={sets} />
+      <SetList />
     </div>
   );
 };
