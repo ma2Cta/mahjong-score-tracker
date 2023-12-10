97a8a5b5bacb7aa19db542577738686cab6ea0ee
ma2Cta <mtstdisk@gmail.com>
2023-12-10 11:11:01 +0900

    Refactor ScoreTable to utilize updated Table components
    The ScoreTable component has been updated to leverage the refreshed Table components from the UI library. A new property 'suppressHover' was introduced to the TableRow component to allow more fine-tuned control over the hover effect. This update enhances overall UI consistency and interaction within the application by aligning the ScoreTable's behavior with other components.

--- app/_components/score-table/ScoreTable.tsx
+++ app/_components/score-table/ScoreTable.tsx
@@ -1,18 +1,18 @@
 import {
-  NoSelectTable,
-  NoSelectTableBody,
-  NoSelectTableCell,
-  NoSelectTableHead,
-  NoSelectTableHeader,
-  NoSelectTableRow,
-} from "@/app/_components/ui/NoSelectTable";
-import {
   childTsumoScores,
   childRonScores,
   parentTsumoScores,
   parentRonScores,
 } from "@/app/_components/score-table/score";
 import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/app/_components/ui/table";
 
 interface ChildScoreTableProps {
   isChild: boolean;
@@ -24,9 +24,7 @@ const ScoreTable: React.FC<ChildScoreTableProps> = ({ isChild, isTsumo }) => {
     let cells = [];
     for (let i = 1; i <= 4; i++) {
       cells.push(
-        <NoSelectTableCell className="w-1/5">
-          {hanValues[i] || "-"}
-        </NoSelectTableCell>,
+        <TableCell className="w-1/5">{hanValues[i] || "-"}</TableCell>,
       );
     }
     return cells;
@@ -44,30 +42,31 @@ const ScoreTable: React.FC<ChildScoreTableProps> = ({ isChild, isTsumo }) => {
 
   return (
     <div className="rounded-md border mt-4">
-      <NoSelectTable>
-        <NoSelectTableHeader>
-          <NoSelectTableRow>
-            <NoSelectTableHead className="w-1/5">符 \ 飜</NoSelectTableHead>
-            <NoSelectTableHead className="w-1/5">1飜</NoSelectTableHead>
-            <NoSelectTableHead className="w-1/5">2飜</NoSelectTableHead>
-            <NoSelectTableHead className="w-1/5">3飜</NoSelectTableHead>
-            <NoSelectTableHead className="w-1/5">4飜</NoSelectTableHead>
-          </NoSelectTableRow>
-        </NoSelectTableHeader>
-        <NoSelectTableBody>
+      <Table>
+        <TableHeader>
+          <TableRow suppressHover={true}>
+            <TableHead className="w-1/5">符 \ 飜</TableHead>
+            <TableHead className="w-1/5">1飜</TableHead>
+            <TableHead className="w-1/5">2飜</TableHead>
+            <TableHead className="w-1/5">3飜</TableHead>
+            <TableHead className="w-1/5">4飜</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
           {Object.entries(selectScores()).map(([fu, hanValues], index) => (
-            <NoSelectTableRow
+            <TableRow
+              suppressHover={true}
               className={index % 2 === 0 ? "bg-muted/50" : ""}
               key={fu}
             >
-              <NoSelectTableCell className="text-muted-foreground w-1/5">
+              <TableCell className="text-muted-foreground w-1/5">
                 {fu}
-              </NoSelectTableCell>
+              </TableCell>
               {createHanValueCells(hanValues)}
-            </NoSelectTableRow>
+            </TableRow>
           ))}
-        </NoSelectTableBody>
-      </NoSelectTable>
+        </TableBody>
+      </Table>
     </div>
   );
 };
--- app/_components/ui/table.tsx
+++ app/_components/ui/table.tsx
@@ -48,19 +48,24 @@ const TableFooter = React.forwardRef<
 ));
 TableFooter.displayName = "TableFooter";
 
-const TableRow = React.forwardRef<
-  HTMLTableRowElement,
-  React.HTMLAttributes<HTMLTableRowElement>
->(({ className, ...props }, ref) => (
-  <tr
-    ref={ref}
-    className={cn(
-      "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
-      className,
-    )}
-    {...props}
-  />
-));
+type TableRowProps = React.HTMLAttributes<HTMLTableRowElement> & {
+  suppressHover?: boolean;
+};
+
+const TableRow = React.forwardRef<HTMLTableRowElement, TableRowProps>(
+  ({ className, suppressHover, ...props }, ref) => (
+    <tr
+      ref={ref}
+      className={cn(
+        "border-b transition-colors",
+        !suppressHover && "hover:bg-muted/50",
+        "data-[state=selected]:bg-muted",
+        className,
+      )}
+      {...props}
+    />
+  ),
+);
 TableRow.displayName = "TableRow";
 
 const TableHead = React.forwardRef<
