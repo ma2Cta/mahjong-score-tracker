df7dd68dd16e0210aea5657043d78b8df07ac0bc
ma2Cta <mtstdisk@gmail.com>
2024-03-09 13:22:24 +0900

    Set isThree to set

--- app/(auth)/sets/create/page.tsx
+++ app/(auth)/sets/create/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import TypographyH2 from "@/app/_components/ui/TypographyH2";
 import CreateSetForm from "@/app/_components/set/CreateSetForm";
 import BreadCrumbs from "@/app/_components/ui/BreadCrumbs";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/app/_components/ui/tabs";
 
 const CreateSet: React.FC = () => {
   const router = useRouter();
@@ -20,7 +25,28 @@ const CreateSet: React.FC = () => {
       />
       <TypographyH2>新しいセットを作成</TypographyH2>
       <div className="my-4">
-        <CreateSetForm onSuccess={() => router.push("/sets")} />
+        <Tabs defaultValue="four" className="w-[400px]">
+          <TabsList>
+            <TabsTrigger value="four">四人麻雀</TabsTrigger>
+            <TabsTrigger value="three">三人麻雀</TabsTrigger>
+          </TabsList>
+          <TabsContent value="four">
+            <div className="my-4">
+              <CreateSetForm
+                onSuccess={() => router.push("/sets")}
+                isThree={false}
+              />
+            </div>
+          </TabsContent>
+          <TabsContent value="three">
+            <div className="my-4">
+              <CreateSetForm
+                onSuccess={() => router.push("/sets")}
+                isThree={true}
+              />
+            </div>
+          </TabsContent>
+        </Tabs>
       </div>
     </>
   );
